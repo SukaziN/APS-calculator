33afa957cfa7e75ecca6ea3e15ac4908cecf6663
SukaziN <nkosiyazisukazie@gmail.com>
2026-02-07 07:21:25 +0200

    created function for select input options

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-//let subjects = [scienceSubjects, commerceSubjects, historySubjects];
-let scienceSubjects = [
-  "Physical Sciences",
-  "Life Sciences",
-  "Geography / Accounting",
-  "Agriculture",
-  "Mathematics",
-  "English",
-  "Home language",
-  "Life Orientation",
-];
-let commerceSubjects = [
-  "Business Studies",
-  "Consumer Studies / Accounting",
-  "Mathematical Literacy",
-  "Economics",
-  "English",
-  "Home language",
-  "Life orientation",
-];
-let historySubjects = [
-  "History",
-  "Geography",
-  "Life Sciences",
-  "Mathematical Literacy",
-  "English",
-  "Home language",
-  "Life Orientation",
-];
-
-let message = "Your career options include but are not limited to: ";
 function calculateAPS() {
   let english = document.getElementById("english").valueAsNumber;
   let homeLang = document.getElementById("home-language").valueAsNumber;
@@ -38,19 +7,57 @@ function calculateAPS() {
   let core3 = document.getElementById("core-3").valueAsNumber;
   let APS = english + homeLang + math + core1 + core2 + core3;
   console.log(APS);
-  switch (
-    subjects //crashes here. FIX IT!
-  ) {
-    case historySubjects:
-      console.log("Your career options include but are not limited to: ");
-    case commerceSubjects:
-      console.log("Your career...");
-  }
+
   if (APS >= 30) {
     console.log("Congratulations on your hard work!");
   } else if (APS < 20) {
     console.log(
       "Congratulations on completing your matriculation! Consider rewriting for better tertiary education prospects.",
     );
+  } else {
+    console.log("Congratulations and best of luck!");
+  }
+}
+function suggestCareer() {
+  let scienceSubjects = [
+    "Physical Sciences",
+    "Life Sciences",
+    "Geography / Accounting",
+    "Agriculture",
+    "Mathematics",
+    "English",
+    "Home language",
+    "Life Orientation",
+  ];
+  let commerceSubjects = [
+    "Business Studies",
+    "Consumer Studies / Accounting",
+    "Mathematical Literacy",
+    "Economics",
+    "English",
+    "Home language",
+    "Life orientation",
+  ];
+  let historySubjects = [
+    "History",
+    "Geography",
+    "Life Sciences",
+    "Mathematical Literacy",
+    "English",
+    "Home language",
+    "Life Orientation",
+  ];
+  scienceSubjects = document.getElementById("science").value;
+  commerceSubjects = document.getElementById("commerce").value;
+  historySubjects = document.getElementById("history").value;
+  let subjects = [scienceSubjects, commerceSubjects, historySubjects];
+  let message = "Your career options include but are not limited to: ";
+  switch (subjects) {
+    case historySubjects:
+      console.log(message);
+    case commerceSubjects:
+      console.log("You would excel in...");
+    case scienceSubjects:
+      console.log("People with your subject combination are eligible for...");
   }
 }
