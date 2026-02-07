bde4620e06edfabe8905b5bf4b592158b19a3a8d
SukaziN <nkosiyazisukazie@gmail.com>
2026-02-07 07:33:48 +0200

    displayed feedback for the user

--- script.js
+++ script.js
@@ -7,19 +7,27 @@ function calculateAPS() {
   let core3 = document.getElementById("core-3").valueAsNumber;
   let APS = english + homeLang + math + core1 + core2 + core3;
   console.log(APS);
+  let results = document.getElementById("aps");
+  results.innerHTML = "APS: " + APS;
+  let encouragement = document.getElementById("encouragement");
 
   if (APS >= 30) {
     console.log("Congratulations on your hard work!");
+    encouragement.innerHTML = "Congratulations on your hard work!";
   } else if (APS < 20) {
     console.log(
       "Congratulations on completing your matriculation! Consider rewriting for better tertiary education prospects.",
     );
+    encouragement.innerHTML =
+      "Congratulations on completing your matriculation! Consider rewriting for better tertiary education prospects.";
   } else {
     console.log("Congratulations and best of luck!");
+    encouragement.innerHTML = "Congratulations and best of luck!";
   }
 }
 function suggestCareer() {
   let scienceSubjects = [
+    //this whole section is overEngineered and doesn't even work. FIX IT!
     "Physical Sciences",
     "Life Sciences",
     "Geography / Accounting",
