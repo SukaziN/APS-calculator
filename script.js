8910c85830e34912e1e2dc725aa9291189e35222
SukaziN <nkosiyazisukazie@gmail.com>
2026-02-08 06:59:25 +0200

    used conditionals to distinguish between clicked elements within the same function

--- script.js
+++ script.js
@@ -25,4 +25,19 @@ function calculateAPS() {
     encouragement.innerHTML = "Congratulations and best of luck!";
   }
 }
-function suggestCareer() {}
+function suggestCareer() {
+  let science = document.getElementById("science");
+  let technical = document.getElementById("technical");
+  let commerce = document.getElementById("commerce");
+  let history = document.getElementById("history");
+  if (science.addEventListener("click")) {
+    //was trying to distinguish clicked elements without having to code different functions. Crashed!
+    console.log("Good choice! Here are some careers you can pursue.");
+  } else if (technical.addEventListener("click")) {
+    console.log("Great choice! people with your skill-set do well in...");
+  } else if (commerce.addEventListener("click")) {
+    console.log("You'd be a great fit at business school!");
+  } else if (history.addEventListener("click")) {
+    console.log("Most people with your subjects excel in ...");
+  }
+}
