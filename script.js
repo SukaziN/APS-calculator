ca336131a1299ef3d4082c089d97ae17dc216fcc
SukaziN <nkosiyazisukazie@gmail.com>
2026-02-08 06:21:42 +0200

    removed over-engineered code and replaced it with a simple list

--- script.js
+++ script.js
@@ -25,47 +25,4 @@ function calculateAPS() {
     encouragement.innerHTML = "Congratulations and best of luck!";
   }
 }
-function suggestCareer() {
-  let scienceSubjects = [
-    //this whole section is overEngineered and doesn't even work. FIX IT!
-    "Physical Sciences",
-    "Life Sciences",
-    "Geography / Accounting",
-    "Agriculture",
-    "Mathematics",
-    "English",
-    "Home language",
-    "Life Orientation",
-  ];
-  let commerceSubjects = [
-    "Business Studies",
-    "Consumer Studies / Accounting",
-    "Mathematical Literacy",
-    "Economics",
-    "English",
-    "Home language",
-    "Life orientation",
-  ];
-  let historySubjects = [
-    "History",
-    "Geography",
-    "Life Sciences",
-    "Mathematical Literacy",
-    "English",
-    "Home language",
-    "Life Orientation",
-  ];
-  scienceSubjects = document.getElementById("science").value;
-  commerceSubjects = document.getElementById("commerce").value;
-  historySubjects = document.getElementById("history").value;
-  let subjects = [scienceSubjects, commerceSubjects, historySubjects];
-  let message = "Your career options include but are not limited to: ";
-  switch (subjects) {
-    case historySubjects:
-      console.log(message);
-    case commerceSubjects:
-      console.log("You would excel in...");
-    case scienceSubjects:
-      console.log("People with your subject combination are eligible for...");
-  }
-}
+function suggestCareer() {}
