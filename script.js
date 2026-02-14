fd9a94dbc5554c66fca262819fe0db067e887976
SukaziN <nkosiyazisukazie@gmail.com>
2026-02-14 15:51:23 +0200

    replaced placeholder careers on template

--- script.js
+++ script.js
@@ -30,58 +30,61 @@ let scienceCareer = document.getElementById("science-careers");
 function suggestScienceCareer() {
   console.log("Good choice! Here are some careers you can pursue.");
   scienceCareer.innerText =
-    "Good choice! Here are some careers you can pursue.";
+    "Good choice! Your career options include, but are not limited to: ";
   let career1 = document.getElementById("choice-1");
   let career2 = document.getElementById("choice-2");
   let career3 = document.getElementById("choice-3");
   let career4 = document.getElementById("choice-4");
   let career5 = document.getElementById("choice-5");
-  career1.innerText = "Civil Engineering";
-  career2.innerText = "Electrical Engineering";
-  career3.innerText = "Mining Engineering";
-  career4.innerText = "Chemical Engineering";
-  career5.innerText = "Medicine";
+  career1.innerText =
+    "Engineering: Civil, Electrical, Mining, Chemical, Mechanical, Metallurgy, Industrial";
+  career2.innerText = "Medicine";
+  career3.innerText = "Pharmacy";
+  career4.innerText = "Computer Science";
+  career5.innerText = "Optometry";
 }
 function suggestTechnicalCareer() {
   console.log("Great choice! people with your skill-set do well in...");
   scienceCareer.innerText =
-    "Great choice! people with your skill-set do well in...";
+    "Great choice! Your career options include, but are not limited to: ";
   let career1 = document.getElementById("choice-1");
   let career2 = document.getElementById("choice-2");
   let career3 = document.getElementById("choice-3");
   let career4 = document.getElementById("choice-4");
   let career5 = document.getElementById("choice-5");
-  career1.innerText = "Civil Engineering";
-  career2.innerText = "Electrical Engineering";
-  career3.innerText = "Mining Engineering";
-  career4.innerText = "Chemical Engineering";
-  career5.innerText = "Medicine";
+  career1.innerText = "Information Systems";
+  career2.innerText = "Information Technology";
+  career3.innerText = "Financial Services Operations";
+  career4.innerText = "Mine Surveying";
+  career5.innerText = "Sport and Exercise Sciences";
 }
 function suggestCommerceCareer() {
   console.log("You'd be a great fit at business school!");
-  scienceCareer.innerText = "You'd be a great fit at business school!";
+  scienceCareer.innerText =
+    "Your career options include, but are not limited to: ";
   let career1 = document.getElementById("choice-1");
   let career2 = document.getElementById("choice-2");
   let career3 = document.getElementById("choice-3");
   let career4 = document.getElementById("choice-4");
   let career5 = document.getElementById("choice-5");
-  career1.innerText = "Civil Engineering";
-  career2.innerText = "Electrical Engineering";
-  career3.innerText = "Mining Engineering";
-  career4.innerText = "Chemical Engineering";
-  career5.innerText = "Medicine";
+  career1.innerText = "Accountancy";
+  career2.innerText = "Human Resource Management";
+  career3.innerText = "Logistics";
+  career4.innerText = "Industrial Psychology";
+  career5.innerText = "Tourism Development and Management";
 }
 function suggestHistoryCareer() {
   console.log("Most people with your subjects excel in ...");
-  scienceCareer.innerText = "Most people with your subjects excel in ...";
+  scienceCareer.innerText =
+    "Your career options include, but are not limited to: ";
   let career1 = document.getElementById("choice-1");
   let career2 = document.getElementById("choice-2");
   let career3 = document.getElementById("choice-3");
   let career4 = document.getElementById("choice-4");
   let career5 = document.getElementById("choice-5");
-  career1.innerText = "Civil Engineering";
-  career2.innerText = "Electrical Engineering";
-  career3.innerText = "Mining Engineering";
-  career4.innerText = "Chemical Engineering";
-  career5.innerText = "Medicine";
+  career1.innerText = "Education";
+  career2.innerText = "Social Work";
+  career3.innerText = "Public Relations and Communication";
+  career4.innerText = "LLB Law";
+  career5.innerText = "Information Management";
 }
